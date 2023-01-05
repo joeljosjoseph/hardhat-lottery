9cb60c2f47d2b6decdcf2eed42f71d473b2cb67e
joeljosjoseph <joeljosjoseph12@gmail.com>
2023-01-05 20:17:26 +0530

    helper-config update

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -14,8 +14,8 @@ const networkConfig = {
         name: "localhost",
         subscriptionId: "588",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
-        keepersUpdateInterval: "30",
-        raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        interval: "30",
+        entranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
     },
 }
